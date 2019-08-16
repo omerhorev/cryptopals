02552af0cd1510506ff9ed85d20d9c0b520ac577
Unknown <omer.hv@gmail.com>
2019-08-16 16:01:34 +0300

    Finished set 2.3

--- include/breaks/moo-detector.h
+++ include/breaks/moo-detector.h
@@ -17,16 +17,16 @@ namespace breaks
          * Returns whether the encrypted data is in ECB mode given we know only the ciphertext.
          * Will work only if the plaintext has the same blocks in it.
          *
-         * @tparam N     The length of the buffers
          * @param cipher The ciphertext
+         * @param length The length of the ciphertext
+         * @param block_size The length of the block
          * @return (bool) True if is in ECB mode, False if not certain (could be, not definitive answer).
          */
-        template<size_t N>
-        static bool is_ecb(const unsigned char (&cipher)[N])
+        static bool is_ecb(const unsigned char cipher[], size_t length, size_t block_size)
         {
-            static_assert(N % 16 == 0, "Must be multiplicand of block size (16)");
+            if(length % 16 != 0) return false;
 
-            auto blocks_count = N / 16;
+            auto blocks_count = length / 16;
 
             // First, lets search for blocks that are equal.
             for (auto i = 0; i < blocks_count; ++i)
@@ -38,7 +38,7 @@ namespace breaks
                     auto a = i * 16;
                     auto b = j * 16;
 
-                    if (memcmp(cipher + a, cipher + b, 16) == 0)
+                    if (memcmp(cipher + a, cipher + b, block_size) == 0)
                     {
                         return true;
                     }
@@ -48,52 +48,6 @@ namespace breaks
             return false;
         }
 
-        /**
-         * Returns whether the encrypted data is in ECB mode given we know the plain but not the key
-         *
-         * @tparam N          The length of the buffers
-         * @param known_plain The known plaintext
-         * @param cipher      The cipher
-         * @return (bool) True if is in ECB mode, False if not certain (could be, not definitive answer).
-         */
-        template<size_t N>
-        static bool is_ecb(const unsigned char (&known_plain)[N], const unsigned char (&cipher)[N])
-        {
-            static_assert(N % 16 == 0, "Must be multiplicand of block size (16)");
-
-            auto blocks_count = N / 16;
-
-            int equal_blocks_found_offset[2];
-            bool found_same_blocks = false;
-
-            // First, lets search for blocks that are equal.
-            for (auto i = 0; i < blocks_count; ++i)
-            {
-                for (auto j = 0; j < blocks_count; ++j)
-                {
-                    if (i == j) continue;
-
-                    auto a = i * 16;
-                    auto b = j * 16;
-
-                    if (memcmp(known_plain + a, known_plain + b, 16) == 0)
-                    {
-                        equal_blocks_found_offset[0] = a;
-                        equal_blocks_found_offset[1] = b;
-                        found_same_blocks = true;
-                    }
-                }
-            }
-
-            if (!found_same_blocks)
-            {
-                return false;
-            }
-
-            // We found two matching blocks, lets check if they were encrypted in the same way
-
-            return memcmp(cipher + equal_blocks_found_offset[0], cipher + equal_blocks_found_offset[1], 16) == 0;
-        }
     };
 }
 
---
+++ tests/sets/2/set2.3.cpp
@@ -0,0 +1,96 @@
+//
+// Created by omerh on 16/08/2019.
+//
+
+#include <gtest/gtest.h>
+#include <random>
+#include <climits>
+#include <model/padding.h>
+#include <breaks/moo-detector.h>
+#include "model/block.h"
+#include "utils/debug.h"
+
+size_t encryption_oracle(unsigned char input[],
+                         size_t length,
+                         unsigned char output[],
+                         size_t output_buffer_length,
+                         bool &was_cbc_selected)
+{
+    std::random_device rd;
+    std::uniform_int_distribution<size_t> length_dist(5, 10);
+    std::uniform_int_distribution<unsigned char> byte_dist(0, 0xff);
+    std::uniform_int_distribution<unsigned char> model_dist(0, 1);
+
+    unsigned char iv[model::aes128_cbc::block_size];
+    unsigned char key[model::aes128_cbc::block_size];
+
+    std::generate_n(iv, sizeof(iv), [&]() {
+        return byte_dist(rd);
+    });
+
+    std::generate_n(key, sizeof(key), [&]() {
+        return byte_dist(rd);
+    });
+
+    size_t rand_length1 = length_dist(rd);
+    size_t rand_length2 = length_dist(rd);
+    size_t total_length = length + rand_length1 + rand_length2;
+    size_t length_after_padding =
+            total_length + model::aes128_cbc::block_size - (total_length % model::aes128_cbc::block_size);
+
+    if (output_buffer_length < length_after_padding)
+    {
+        return 0;
+    }
+
+    std::generate_n(output, rand_length1, [&]() {
+        return byte_dist(rd);
+    });
+
+    std::copy_n(input, length, output + rand_length1);
+
+    std::generate_n(output + rand_length1 + length, rand_length2, [&]() {
+        return byte_dist(rd);
+    });
+
+    auto padded_length = model::padding::pkcs7::encode(output,
+                                                       output_buffer_length,
+                                                       total_length,
+                                                       model::aes128_cbc::block_size);
+
+    if (model_dist(rd) == 1)
+    {
+        was_cbc_selected = false;
+        model::aes128_ecb m;
+        m.initialize(key, sizeof(key));
+        m.encrypt(output, padded_length);
+    }
+    else
+    {
+        was_cbc_selected = true;
+        model::aes128_cbc m;
+        m.initialize(key, sizeof(key), iv, sizeof(iv));
+        m.encrypt(output, padded_length);
+    }
+
+    return padded_length;
+}
+
+TEST(sets, set_2_3)
+{
+    const size_t block_size = model::aes128_cbc::block_size;
+
+    unsigned char input[block_size * 3] = {0};
+    unsigned char ciphertext[block_size * 5] = {0};
+    bool was_cbc_selected;
+
+    for (int i = 0; i < 100; ++i)
+    {
+        auto length = encryption_oracle(input, sizeof(input), ciphertext, sizeof(ciphertext), was_cbc_selected);
+        bool is_ecb_selected = !was_cbc_selected;
+
+        ASSERT_EQ(breaks::moo_detector::is_ecb(ciphertext, length, model::aes128_cbc::block_size), is_ecb_selected);
+    }
+
+
+}
